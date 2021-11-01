bd48e17d0a432dbdcfae4268bb7a874b5475b3be
I Komang Arisudan <arisudanadrn.id@gmail.com>
2021-11-01 16:06:32 +0800

    add overloaded function

--- src/com/komangmkpl/RandomNumberGener.java
+++ src/com/komangmkpl/RandomNumberGener.java
@@ -6,4 +6,8 @@ public class RandomNumberGener {
     public int getRandomNumber(){
         return new Random().nextInt();
     }
+
+    public int getRandomNumber(int limit){
+        return new Random().nextInt(limit);
+    }
 }
